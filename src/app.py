323393f3616c2855c8010efa8e48b5d430dd697c
Aygren <aygren@gmail.com>
2026-06-22 21:40:15 +0300

    Установка пути для импорта через sys.path

--- src/app.py
+++ src/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 import threading
-from . import scraper
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__))) # Добавляет текущую папку (src) в пути поиска
+
+import scraper
 
 app = Flask(__name__)
 
