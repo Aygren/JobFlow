1bdbc9b5584e9c575ba48f707b772afc09955b17
Aygren <aygren@gmail.com>
2026-06-22 20:53:26 +0300

    Добавление Flask-сервера для интеграции с Cron-job

---
+++ src/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import threading
+import scraper
+
+app = Flask(__name__)
+
+@app.route('/run')
+def run_job():
+    # Запускаем парсер в отдельном потоке, чтобы не блокировать веб-сервер
+    threading.Thread(target=scraper.run_scraper).start()
+    return "Парсер запущен!", 200
+
+@app.route('/')
+def health_check():
+    return "Бот готов к работе", 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
